5e595ab877ae47c408e1d943cb6f4952509e430f
Brad <brad@debian.localdomain>
2026-02-22 17:09:28 +0700

    feat:Add hyperparameters support for model

--- cirrhosis_patient_survival_prediction/model.py
+++ cirrhosis_patient_survival_prediction/model.py
@@ -1,15 +1,17 @@
-import argparse
 import os
 import joblib
 import logging
 import pandas as pd
+from sklearn.metrics import log_loss
 from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
 from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
 from fire import Fire
+from clearml import Task, OutputModel
 
 
 class My_Classifier_Model:
@@ -17,18 +19,30 @@ class My_Classifier_Model:
     Wraps any sklearn pipeline for training, saving, loading, and predicting.
     The pipeline should include preprocessing + model.
     """
-    
+
     logfile = "./data/log_file.log"
     results_file = "./data/results.csv"
     classifier_file = "./model/pipeline.pkl"
-    
 
-    def __init__(self, pipeline=None):
+    default_hyperparameters = {
+        "n_estimators": 1000,
+        "learning_rate": 0.01,
+        "max_depth": 6,
+        "subsample": 0.8,
+        "colsample_bytree": 0.8,
+        "gamma": 1,
+    }
+
+    def __init__(self, hyperparameters=None):
         """
         pipeline: any sklearn Pipeline object (preprocessing + estimator)
         """
-        self.pipeline = pipeline
-        
+
+        if hyperparameters is None:
+            hyperparameters = self.default_hyperparameters
+
+        self.hyperparameters = hyperparameters
+
         os.makedirs("./model", exist_ok=True)
         os.makedirs("./data", exist_ok=True)
 
@@ -38,65 +52,64 @@ class My_Classifier_Model:
             format="%(asctime)s - %(levelname)s - %(message)s",
         )
 
-    def _build_default_pipeline(self, dataset_path):
-        """
-        Automatically detect numerical and categorical columns
-        from dataset and build pipeline.
-        """
-
-        data = pd.read_csv(dataset_path, index_col=0)
-        
-        data = data.drop(['Status'], axis=1)
-
-        categorical_cols = data.select_dtypes(include=["str"]).columns.tolist()
-        numerical_cols = data.select_dtypes(include=["int64", "float64"]).columns.tolist()
-
-        preprocessor = ColumnTransformer(
-            transformers=[
-                ("num", SimpleImputer(strategy="constant"), numerical_cols),
-                ("cat", Pipeline([
-                    ("imputer", SimpleImputer(strategy="constant")),
-                    ("onehot", OneHotEncoder(handle_unknown="ignore"))
-                ]), categorical_cols)
-            ]
-        )
-
-        pipeline = Pipeline([
-            ("preprocessor", preprocessor),
-            ("model", XGBClassifier(
-                n_estimators=1000,
-                learning_rate=0.01,
-                random_state=0,
-                eval_metric="logloss"
-            ))
-        ])
-
-        return pipeline
-
     def train(self, dataset):
-        """
-        Train pipeline on dataset and save artifacts
-        """
-        try: 
+        try:
             data = pd.read_csv(dataset, index_col=0)
 
             self.label_encoder = LabelEncoder()
             y = self.label_encoder.fit_transform(data["Status"])
             X = data.drop(["Status"], axis=1)
 
-            # Train pipeline
-            if self.pipeline is None:
-                self.pipeline = self._build_default_pipeline(dataset)
-            self.pipeline.fit(X, y)
+            X_train, X_valid, y_train, y_valid = train_test_split(
+                X, y, test_size=0.2, random_state=42
+            )
 
-            # Save artifacts
-            joblib.dump(self.pipeline, self.classifier_file)
-            logging.info(f"Trained model on {dataset}")
-            
+            categorical_cols = X.select_dtypes(include=["str"]).columns.tolist()
+            numerical_cols = X.select_dtypes(
+                include=["int64", "float64"]
+            ).columns.tolist()
+
+            preprocessor = ColumnTransformer(
+                transformers=[
+                    ("num", SimpleImputer(strategy="constant"), numerical_cols),
+                    (
+                        "cat",
+                        Pipeline(
+                            [
+                                ("imputer", SimpleImputer(strategy="constant")),
+                                ("onehot", OneHotEncoder(handle_unknown="ignore")),
+                            ]
+                        ),
+                        categorical_cols,
+                    ),
+                ]
+            )
+
+
+            pipeline = Pipeline(
+                [
+                    ("preprocessor", preprocessor),
+                    (
+                        "model",
+                        XGBClassifier(
+                            random_state=42,
+                            **self.hyperparameters,
+                            eval_metric="logloss",
+                        ),
+                    ),
+                ]
+            )
+
+            pipeline.fit(X_train, y_train)
+
+            logging.info(f"Model trained with hyperparameters: {self.hyperparameters}")
+
+            joblib.dump(pipeline, self.classifier_file)
+
+
         except Exception as e:
             logging.error(f"Error during training: {e}")
             raise e
-        
 
     def predict(self, dataset):
         """
@@ -104,27 +117,23 @@ class My_Classifier_Model:
         """
         try:
             pipeline = joblib.load(self.classifier_file)
-            X_test = pd.read_csv(
-                dataset, index_col=0
-            )
-            
+            X_test = pd.read_csv(dataset, index_col=0)
+
             X_test_ids = X_test.index
 
             test_preds_proba = pipeline.predict_proba(X_test)
-            
+
             class_names = [f"Status_{i}" for i in range(test_preds_proba.shape[1])]
             output = pd.DataFrame(test_preds_proba, columns=class_names)
-            
+
             output.insert(0, "id", X_test_ids)
             output.to_csv(self.results_file, index=False)
-            logging.info(f"Predicted on {dataset} and saved results to {self.results_file}")
+            logging.info(
+                f"Predicted on {dataset} and saved results to {self.results_file}"
+            )
         except Exception as e:
             logging.error(f"Error during prediction: {e}")
             raise e
-        
-
-
-
 
 
 if __name__ == "__main__":
